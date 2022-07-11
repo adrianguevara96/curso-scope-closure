cc9220d4fc5e5931592c7a582f1ed3215b3813c2
Adrian Guevara <adrianjga96@gmail.com>
2022-07-11 15:01:24 -0400

    clase 5 - use strict mode

---
+++ src/scope/c5 - strict mode.js
@@ -0,0 +1,13 @@
+//STRICT MODE
+//permite ejecutar de formar estricta unas reglas para tener mejor control
+//y cumplir los estandares
+'use strict';
+pi = 3.1416;
+console.log(pi);
+
+function myFunction() {
+    'use strict';
+    return pi = 3.1416;
+}
+
+console.log(myFunction());
